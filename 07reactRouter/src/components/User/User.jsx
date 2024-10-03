33a83a4dd05e57c2499f99a0e518f4ccd3a01d80
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-03 23:24:20 +0530

    create 07reactRouter folder

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+
+
+function User() {
+    const {userid} = useParams();
+
+    return (
+        <>
+        <div className='text-center'>User : {userid}</div>
+       
+        </>
+  )
+}
+
+export default User
