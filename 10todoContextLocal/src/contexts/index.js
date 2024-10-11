79bdb48f169700e753b32a83ffb2b60fb2336789
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-11 21:07:38 +0530

    adding 10Todocontext

---
+++ 10todoContextLocal/src/contexts/index.js
@@ -0,0 +1,5 @@
+import { TodoContext } from "./todoContext";
+import { TodoProvider } from "./todoContext";
+import { useTodo } from "./todoContext";
+
+export default {TodoContext,TodoProvider,useTodo};
