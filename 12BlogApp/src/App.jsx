50b6ed5f6d2fb0d6247337f074974450489a1f10
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-25 21:35:10 +0530

    adding BlogApp Setup files of appwrite

---
+++ 12BlogApp/src/App.jsx
@@ -0,0 +1,18 @@
+ 
+import './App.css'
+
+
+
+function App() {
+   console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+    <div>
+      <h1>A Blog App with AppWrite | VikasDewan</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
